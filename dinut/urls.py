b403e7d3a29f5ec1bf3c139c052cf8a3beac90a5
minyeamer <minyeamer@gmail.com>
2022-05-24 16:08:38 +0900

    start diet app

--- dinut/urls.py
+++ dinut/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('account/', include('accountapp.urls')),
     path('profile/', include('profileapp.urls')),
     path('food/', include('foodapp.urls')),
+    path('diet/', include('dietapp.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

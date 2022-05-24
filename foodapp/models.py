403517d9f48df270a0d21862e08465b92afecc31
minyeamer <minyeamer@gmail.com>
2022-05-24 09:33:10 +0900

    add missing model

--- foodapp/models.py
+++ foodapp/models.py
@@ -1,3 +1,11 @@
 # Create your models here.
 from django.db import models
 
+class ImageUploadModel(models.Model):
+    #uid = 
+    #upload_date =  models.DateTimeField(auto_now = True)
+    foodImage = models.ImageField(upload_to='images/%Y/%m/%d')
+    #morning_food = 
+    #afternoon_food = 
+    #dinner_food =
+    #dissert =

9e2e8eb315136a7f180c0da041059f2ccd49158b
minyeamer <minyeamer@gmail.com>
2022-05-26 01:45:46 +0900

    add diet.csv and rename script file

--- script.py
+++ script.py
@@ -5,11 +5,12 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dinut.settings")
 django.setup()
 
-from foodapp.models import Nutrition
+from dietapp.models import Nutrition, Diet
 
-CSV_PATH_PRODUCTS='script/nutrition.csv'
+NUTRITION_DB = 'script/nutrition.csv'
+DIET_DB = 'script/diet.csv'
 
-with open(CSV_PATH_PRODUCTS) as nutrition_csv:
+with open(NUTRITION_DB) as nutrition_csv:
     rows = csv.reader(nutrition_csv)
     next(rows, None)
     for row in rows:
@@ -25,4 +26,20 @@ with open(CSV_PATH_PRODUCTS) as nutrition_csv:
             sugar = row[9],
             fiber = row[10],
             sodium = row[11],
+        )
+
+with open(DIET_DB) as diet_csv:
+    rows = csv.reader(diet_csv)
+    next(rows, None)
+    for row in rows:
+        Diet.objects.create(
+            food_list = row[1],
+            energy = row[2],
+            carb = row[3],
+            protein = row[4],
+            fat = row[5],
+            vitamin_a = row[6],
+            riboflavin = row[7],
+            calcium = row[8],
+            iron = row[9],
         )

509d25d127bbda57fc49dea0b1ed8a111e22ecb8
minyeamer <kimmy9809@gmail.com>
2022-05-25 14:23:04 +0900

    add nutrition.csv to db

--- foodapp/admin.py
+++ foodapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from foodapp.models import FoodImage
+from foodapp.models import FoodImage, Nutrition
 
 # Register your models here.
 class FoodImageAdmin(admin.ModelAdmin):
     pass
+class NutritionAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(FoodImage, FoodImageAdmin)
+admin.site.register(Nutrition, NutritionAdmin)
--- foodapp/models.py
+++ foodapp/models.py
@@ -10,13 +10,16 @@ class FoodImage(models.Model):
 
 class Nutrition(models.Model):
     food_name = models.CharField('식품명', max_length=255, unique=True)
-    product_name = models.CharField('상품명', max_length=255, unique=True)
-    main_cat = models.CharField('식품대분류', max_length=255, unique=True)
-    sub_cat = models.CharField('식품상세분류', max_length=255, unique=True)
-    energy = models.PositiveIntegerField('에너지(kcal)', default=0)
-    carb = models.PositiveIntegerField('탄수화물(g)', default=0)
-    protein = models.PositiveIntegerField('단백질(g)', default=0)
-    fat = models.PositiveIntegerField('지방(g)', default=0)
-    sugar = models.PositiveIntegerField('당류(g)', default=0)
-    fiber = models.PositiveIntegerField('식이섬유(g)', default=0)
-    sodium = models.PositiveIntegerField('나트륨(mg)', default=0)
+    product_name = models.CharField('상품명', max_length=255)
+    main_cat = models.CharField('식품대분류', max_length=255)
+    sub_cat = models.CharField('식품상세분류', max_length=255)
+    energy = models.FloatField('에너지(kcal)', default=0)
+    carb = models.FloatField('탄수화물(g)', default=0)
+    protein = models.FloatField('단백질(g)', default=0)
+    fat = models.FloatField('지방(g)', default=0)
+    sugar = models.FloatField('당류(g)', default=0)
+    fiber = models.FloatField('식이섬유(g)', default=0)
+    sodium = models.FloatField('나트륨(mg)', default=0)
+
+    def __str__(self):
+        return f'{self.food_name} - {self.energy} kcal'

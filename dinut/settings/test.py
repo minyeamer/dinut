03eb19033de7b6240c128cc723e7713a3072b936
minyeamer <minyeamer@gmail.com>
2022-05-29 18:28:35 +0900

    for test

--- dinut/settings/test.py
+++ dinut/settings/test.py
@@ -1,14 +1,6 @@
 from .base import *
 
 
-def read_secret(secret_name):
-    file = open('/run/secrets/' + secret_name)
-    secret = file.read()
-    secret = secret.rstrip().lstrip()
-    file.close()
-    return secret
-
-
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -22,25 +14,21 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
-        'HOST': 'mariadb',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -51,10 +39,14 @@ MODEL_ROOT = os.path.join(BASE_DIR, 'model')
 
 DL_MODELS = dict()
 
+print(os.listdir(BASE_DIR))
+print(os.listdir(MODEL_ROOT))
+
 from torch import hub
 # try:
 # load yolov5, if yolov5 repository and weight exist in local
 YOLO_HOME = os.path.join(MODEL_ROOT, 'ultralytics/yolov5')
+print(os.listdir(YOLO_HOME))
 YOLO_WEIGHT = os.path.join(MODEL_ROOT, 'yolov5s.pt')
 hub.set_dir(MODEL_ROOT)
 DL_MODELS['YOLOv5'] = hub.load(YOLO_HOME, model='custom', source='local', path=YOLO_WEIGHT)
@@ -63,14 +55,9 @@ DL_MODELS['YOLOv5'] = hub.load(YOLO_HOME, model='custom', source='local', path=Y
 #     print('yolov5 model not found')
 #     DL_MODELS['YOLOv5'] = hub.load('ultralytics/yolov5', 'yolov5s.pt')
 
-try:
-    # load keras model, if keras model exists in local
-    from tensorflow.keras.models import load_model as load_keras_model
-    DL_MODELS['InceptionV3'] = load_keras_model(os.path.join(MODEL_ROOT, 'inceptionv3.h5'))
-except:
-    # if not, notice
-    print('keras model not found')
-    DL_MODELS['InceptionV3'] = None
+# load keras model, if keras model exists in local
+from tensorflow.keras.models import load_model as load_keras_model
+DL_MODELS['InceptionV3'] = load_keras_model(os.path.join(MODEL_ROOT, 'inceptionv3.h5'))
 
 DB_DIR = os.path.join(BASE_DIR, 'script')
 LABEL_PATH = os.path.join(DB_DIR, 'label.json')

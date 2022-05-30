456a15c633ca6d70ebec818aebe340647f8697eb
minyeamer <minyeamer@gmail.com>
2022-05-31 02:35:15 +0900

    append front design

--- dietapp/urls.py
+++ dietapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from dietapp.views import DietUploadView, DailyDietView
+from dietapp.views import DietUploadView, DailyDietView, home
 
 app_name = 'dietapp'
 
 urlpatterns = [
+    path('home/',  home, name='home'),
     path('upload/', DietUploadView.as_view(), name='upload'),
     path('daily/', DailyDietView.index, name='daily'),
     path('daily/detail/', DailyDietView.detail, name='detail'),

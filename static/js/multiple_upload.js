6b7862469576ef542ccf1bcb274d826faa82ec78
minyeamer <minyeamer@gmail.com>
2022-05-31 09:45:41 +0900

    set upload to take only one picture

--- static/js/multiple_upload.js
+++ static/js/multiple_upload.js
@@ -41,11 +41,15 @@ function DropFile(dropAreaId, fileListId, previewId, deleteFlag) {
     }
 
   //드래그된 파일로 대체해주기
+  if(!fileValidation(files, files[0].name)){
+    return false;
+  }
     var delete_target = document.getElementById(deleteFlag).value
     delete_target = 'False';
+    
     var $file = document.getElementById(fileListId);
-    $file.files = files
-    renderFile(files)
+    $file.files = files;
+    renderFile(files);
   }
 
   //각각의 파일 미리보기 렌더링
@@ -70,98 +74,122 @@ function DropFile(dropAreaId, fileListId, previewId, deleteFlag) {
   dropArea.addEventListener("dragleave", unhighlight, false);
   dropArea.addEventListener("drop", handleDrop, false);
 
- }
-  const dropAreaList = ['drop-file-morning','drop-file-lunch','drop-file-dinner','drop-file-snack'];
-  const fileList = ['morning_diet','lunch_diet','dinner_diet','snack_diet'];
-  const previewList = ['morining-pre','lunch-pre','dinner-pre','snack-pre'];
-  const deleteFlag = ['morning_delete_flag','lunch_delete_flag','dinner_delete_flag','snack_delete_flag'];
+}
+const dropAreaList = ['drop-file-morning','drop-file-lunch','drop-file-dinner','drop-file-snack'];
+const fileList = ['morning_diet','lunch_diet','dinner_diet','snack_diet'];
+const previewList = ['morining-pre','lunch-pre','dinner-pre','snack-pre'];
+const deleteFlag = ['morning_delete_flag','lunch_delete_flag','dinner_delete_flag','snack_delete_flag'];
 
-  for (let i=0; i<dropAreaList.length; i++){
-      var dropFile = new DropFile(dropAreaList[i], fileList[i], previewList[i], deleteFlag[i]);
+for (let i=0; i<dropAreaList.length; i++){
+    var dropFile = new DropFile(dropAreaList[i], fileList[i], previewList[i], deleteFlag[i]);
+}
+
+function inputRenderFile(id, files){
+  if(!fileValidation(files, files[0].name)){
+    return false;
   }
 
-  function inputRenderFile(id, files){
-    var target = null;
-    const reader = new FileReader();
+  var target = null;
+  const reader = new FileReader();
 
-    if (id == 'morning_diet'){
-      target = document.getElementById(previewList[0]);
-      document.getElementById(deleteFlag[0]).value = 'False'
+  if (id == 'morning_diet'){
+    target = document.getElementById(previewList[0]);
+    document.getElementById(deleteFlag[0]).value = 'False'
 
-    }else if(id == 'lunch_diet'){
-      target = document.getElementById(previewList[1]);
-      document.getElementById(deleteFlag[1]).value = 'False'
+  }else if(id == 'lunch_diet'){
+    target = document.getElementById(previewList[1]);
+    document.getElementById(deleteFlag[1]).value = 'False'
 
-    }else if(id == 'dinner_diet'){
-      target = document.getElementById(previewList[2]);
-      document.getElementById(deleteFlag[2]).value = 'False'
+  }else if(id == 'dinner_diet'){
+    target = document.getElementById(previewList[2]);
+    document.getElementById(deleteFlag[2]).value = 'False'
 
-    }else{
-      target = document.getElementById(previewList[3]);
-      document.getElementById(deleteFlag[3]).value = 'False'
-    }
-    deleteChilds(target);
-    reader.addEventListener('load', () => {
-    const img = document.createElement('IMG');
-    img.classList.add('thumbnail');
-    img.src = reader.result;
-    img.setAttribute("width", "150px");
+  }else{
+    target = document.getElementById(previewList[3]);
+    document.getElementById(deleteFlag[3]).value = 'False'
+  }
+
+  deleteChilds(target);
+  reader.addEventListener('load', () => {
+  const img = document.createElement('IMG');
+  img.classList.add('thumbnail');
+  img.src = reader.result;
+  img.setAttribute("width", "150px");
+  
+  target.insertAdjacentElement('beforeend', img);
+  });
+    reader.readAsDataURL(files[0]);
+}
+
+function deleteImage(id){
+  var target = null;
+  var $file = null;
+
+  if (id == 'delete_morning_diet'){
+    document.getElementById(deleteFlag[0]).value = 'True';
+    target = document.getElementById(previewList[0]);
+    $file = document.getElementById(fileList[0]);
+
+  }else if(id == 'delete_lunch_diet'){
+    document.getElementById(deleteFlag[1]).value = 'True';
+    target = document.getElementById(previewList[1]);
+    $file = document.getElementById(fileList[1]);
+
+  }else if(id == 'delete_dinner_diet'){
+    document.getElementById(deleteFlag[2]).value = 'True';
+    target = document.getElementById(previewList[2]);
+    $file = document.getElementById(fileList[2]);
+
+  }else{ // snack
+    document.getElementById(deleteFlag[3]).value = 'True';
+    target = document.getElementById(previewList[3]);
+    $file = document.getElementById(fileList[3]);
+  }
+  deleteChilds(target);
+  $file.files = null;
+  $file.select();
+  document.selection.clear();
+}
+
+function validationCheck(){
+  const form = document.getElementById('daily_form');
+
+  if(document.getElementById(fileList[0]).value){
+    form.submit();
+  }
+  else if(document.getElementById(fileList[1]).value){
+    form.submit();
+  }
+  else if(document.getElementById(fileList[2]).value){
+    form.submit();
+  }
+  else if(document.getElementById(fileList[3]).value){
+    form.submit();
+  }
+  else {
+    alert("파일을 1개 이상 첨부해주세요.");
+    return false;
+  }
+}
+
+function fileValidation(file, fileName){
+  /* 파일 크기 검사 */
+  var fileSize = file.size;
+  var maxSize = 3 * 1024 * 1024; //3mb  // 1024*1024 = 1048579 = 1mb
+  
+  // 1) 파일용량 체크
+  if(fileSize > maxSize){ // 용량 초과시
+    var msg = maxSize / 1048576 + "MB 이하의 파일만 업로드 가능합니다.";
+    alert(msg);
+    return false;
+  }
     
-    target.insertAdjacentElement('beforeend', img);
-    });
-      reader.readAsDataURL(files[0]);
-  }
-
-  function deleteImage(id){
-    var target = null;
-    var $file = null;
-
-    if (id == 'delete_morning_diet'){
-      document.getElementById(deleteFlag[0]).value = 'True';
-      target = document.getElementById(previewList[0]);
-      $file = document.getElementById(fileList[0]);
-
-    }else if(id == 'delete_lunch_diet'){
-      document.getElementById(deleteFlag[1]).value = 'True';
-      target = document.getElementById(previewList[1]);
-      $file = document.getElementById(fileList[1]);
-
-    }else if(id == 'delete_dinner_diet'){
-      document.getElementById(deleteFlag[2]).value = 'True';
-      target = document.getElementById(previewList[2]);
-      $file = document.getElementById(fileList[2]);
-
-    }else{ // snack
-      document.getElementById(deleteFlag[3]).value = 'True';
-      target = document.getElementById(previewList[3]);
-      $file = document.getElementById(fileList[3]);
+  if (fileName != "") {
+    var ext = fileName.slice(fileName.lastIndexOf(".") + 1).toLowerCase();
+    if (!(ext == "gif" || ext == "jpg" || ext == "png")) {
+        alert("이미지파일 (.jpg, .png, .gif ) 만 업로드 가능합니다.");
+        return false;
     }
-    deleteChilds(target);
-    $file.files = null;
-    $file.select();
-    document.selection.clear();
-  }
-
-  function validationCheck(){
-    const form = document.getElementById('daily_form');
-
-    if(document.getElementById(fileList[0]).value){
-      form.submit();
-    }
-    else if(document.getElementById(fileList[1]).value){
-      form.submit();
-    }
-    else if(document.getElementById(fileList[2]).value){
-      form.submit();
-    }
-    else if(document.getElementById(fileList[3]).value){
-      form.submit();
-    }
-    else {
-      alert("파일을 1개 이상 첨부해주세요.");
-      return false;
-    }
-  }
-
-
-
+}
+  return true;
+}
--- static/js/upload.js
+++ static/js/upload.js
@@ -28,31 +28,58 @@ function DropFile(dropAreaId, fileListId) {
 
     handleFiles(files);
 
-    const fileList = document.getElementById(fileListId);
+    const fileList = document.getElementById(fileListId).val;
     if (fileList) {
       fileList.scrollTo({ top: fileList.scrollHeight });
     }
-
-  //드래그된 파일로 대체해주기 
-    var $file = document.getElementById("chooseFile")
-    $file.files = files
-
   }
-
+  
   //html에 표시 해줄 파일 배열 추출 
   function handleFiles(files) {
+    var $file = document.getElementById("chooseFile");
+    
+    if(!fileValidation(files, files[0].name)){
+      return false;
+    }
+
+    let tumbnail = document.querySelector('.thumbnail');
+    let details =  document.querySelector('.details');
+    let title =  document.getElementById("message_text");
+    if (title != null){
+      title.remove();
+    }
+
+    if ( tumbnail != null || details != null){
+      var fileArea = document.querySelector('#fileArea');
+      fileArea.parentNode.removeChild(fileArea);
+      $file.files = null;
+    }
+
+    LoadImg(files);
+    
+    $file.files = files;
     files = [...files];
     files.forEach(previewFile);
   }
-
-  function previewFile(file) {
-    console.log(file);
-    fileList.appendChild(renderFile(file));
+  
+  function LoadImg(files) {
+    var reader = new FileReader();
+    var previewImage = document.getElementById("previewImage");
+      reader.addEventListener('load', () => {
+      previewImage.setAttribute("width", "300px");
+      previewImage.src = reader.result;
+      });
+      reader.readAsDataURL(files[0]);
   }
 
+  function previewFile(file) {
+    fileList.appendChild(renderFile(file));
+  }
+  
   function renderFile(file) {
     let fileDOM = document.createElement("div");
     fileDOM.className = "file";
+    fileDOM.id ='fileArea';
     fileDOM.innerHTML = `
       <div class="thumbnail">
         <img src="https://img.icons8.com/pastel-glyph/2x/image-file.png" alt="파일타입 이미지" class="image">
@@ -85,3 +112,25 @@ function DropFile(dropAreaId, fileListId) {
 }
 
 const dropFile = new DropFile("drop-file", "files");
+
+function fileValidation(file, fileValue){
+  /* 파일 크기 검사 */
+  var fileSize = file.size;
+  var maxSize = 3 * 1024 * 1024; //3mb  // 1024*1024 = 1048579 = 1mb
+  
+  // 1) 파일용량 체크
+  if(fileSize > maxSize){ // 용량 초과시
+    var msg = maxSize / 1048576 + "MB 이하의 파일만 업로드 가능합니다.";
+    alert(msg);
+    return false;
+  }
+    
+  if (fileValue != "") {
+    var ext = fileValue.slice(fileValue.lastIndexOf(".") + 1).toLowerCase();
+    if (!(ext == "gif" || ext == "jpg" || ext == "png")) {
+        alert("이미지파일 (.jpg, .png, .gif ) 만 업로드 가능합니다.");
+        return false;
+    }
+}
+  return true;
+}
